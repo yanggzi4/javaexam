8084170c9235f40cf0b7b5f779233d82b857c7c8
Mingma Sherpa <mingmasherpa@mingmas-air.myfiosgateway.com>
2022-06-11 01:24:42 -0400

    problems

--- src/problems/string/DetermineLargestWord.java
+++ src/problems/string/DetermineLargestWord.java
@@ -13,19 +13,63 @@ public class DetermineLargestWord {
     public static void main(String[] args) {
 
         String str = "Human brain is a biological learning machine";
-          String [] word= str.split("");
-          String largestWord= "";
-        for(int i = 0; i < word.length; i++){
-            if(word[i].length() >= largestWord.length()){
-                largestWord = word[i];
+        str += " "; // add a space at the end of the string
+
+        String[] words = new String[50];
+
+        int length = 0; // to keep track of the length of the words
+
+        String word = "";
+
+        String largestWord = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            // Split the string into words
+
+            if (str.charAt(i) != ' ') {
+
+                word = word + str.charAt(i);
+
+            } else {
+
+                //Add word to the array
+
+                words[length] = word;
+
+                length++; //
+
+                // set the word to an empty string
+
+                word = "";
+
             }
-            System.out.print(largestWord);
+
         }
 
+        int lengthOfLargestWord = 0;
+
+        largestWord = words[0]; // set the largest word to start as the first element in the words array
+
+        for (int j = 0; j < length; j++) {
+
+            if (largestWord.length() < words[j].length())
+
+                largestWord = words[j];
+
+            lengthOfLargestWord = largestWord.length();
+
+        }
+
+        System.out.println("Length of the longest word: " + lengthOfLargestWord);
+
+        System.out.println("Largest word: " + largestWord);
+
+    }
 
 
         //write your code here
 
-    }
+
 
 }
--- src/problems/string/DuplicateWord.java
+++ src/problems/string/DuplicateWord.java
@@ -16,9 +16,32 @@ public class DuplicateWord {
     public static void main(String[] args) {
 
         String str = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language, Java is also coffee";
+        int count;
+        String duplicates = " ";
+        String[] words = str.toLowerCase().trim().split(" ");
+        for (int i = 0; i < words.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    count++;
+                }
+            }
+        }
+        if (count > 1) {
+            int i;
+            if (duplicates.isEmpty()) {
+                duplicates += " " + words[i] + " : " + count;
+            } else if (!duplicates.matches("(.*) " + words[i] + " (.*)")) {
+                duplicates += " " + words[i] + " : " + count;
+            }
+        }
+    }
+     System.out.println( duplicates);
+    
+
 
         //run your code here
 
 
-    }
+
 }
